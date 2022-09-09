92b7cef226e364c2165e51f76b125db4b25c9c78
walumbe <nathanwalumbe@gmail.com>
2022-09-09 18:30:15 +0300

    retain form input values

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -18,7 +18,7 @@
             <input
                 type="text"
                 class="border border-gray-200 rounded p-2 w-full"
-                name="company"/>
+                name="company" value="{{old('company')}}"/>
                 @error('company')
                     <p class="text-red-500 text-xs mt-1">{{$message}}</p>
                 @enderror
@@ -32,7 +32,7 @@
                 type="text"
                 class="border border-gray-200 rounded p-2 w-full"
                 name="title"
-                placeholder="Example: Senior Laravel Developer"
+                placeholder="Example: Senior Laravel Developer" value="{{old('title')}}"
             />
             @error('title')
                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -49,7 +49,7 @@
                 type="text"
                 class="border border-gray-200 rounded p-2 w-full"
                 name="location"
-                placeholder="Example: Remote, Boston MA, etc"/>
+                placeholder="Example: Remote, Boston MA, etc" value="{{old('location')}}"/>
                 @error('location')
                     <p class="text-red-500 text-xs mt-1">{{$message}}</p>
                 @enderror
@@ -62,7 +62,7 @@
             <input
                 type="text"
                 class="border border-gray-200 rounded p-2 w-full"
-                name="email"/>
+                name="email" value="{{old('email')}}"/>
                 @error('email')
                     <p class="text-red-500 text-xs mt-1">{{$message}}</p>
                 @enderror
@@ -78,7 +78,7 @@
             <input
                 type="text"
                 class="border border-gray-200 rounded p-2 w-full"
-                name="website"/>
+                name="website" value="{{old('website')}}"/>
                 @error('website')
                     <p class="text-red-500 text-xs mt-1">{{$message}}</p>
                 @enderror
@@ -92,7 +92,7 @@
                 type="text"
                 class="border border-gray-200 rounded p-2 w-full"
                 name="tags"
-                placeholder="Example: Laravel, Backend, Postgres, etc"
+                placeholder="Example: Laravel, Backend, Postgres, etc" value="{{old('tags')}}"
             />
             @error('tags')
                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -121,7 +121,7 @@
                 class="border border-gray-200 rounded p-2 w-full"
                 name="description"
                 rows="10"
-                placeholder="Include tasks, requirements, salary, etc"
+                placeholder="Include tasks, requirements, salary, etc" value="{{old('description')}}"
             ></textarea>
             @error('description')
                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>

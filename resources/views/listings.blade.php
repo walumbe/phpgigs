821f5991ca09e120293d28b9b9ba6fddb5f4f7db
walumbe <nathanwalumbe@gmail.com>
2022-09-09 11:33:54 +0300

    components and tags in view

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -2,21 +2,15 @@
 
 @section('content')
 
-<h1>{{$heading}}</h1>
-{{-- @if(count($listings == 0))
-<p>No Listings</p>
-@endif --}}
+@include('partials._hero')
+@include('partials._search')
+
+<div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 
 @unless (count($listings) == 0)
     
 @foreach($listings as $listing)
-<h2>
-    <a href="/listings/{{$listing['id']}}">
-    {{$listing['title']}}</a>
-</h2>
-<p>
-    {{$listing['description']}}
-</p>
+    <x-listing-card :listing="$listing"/>
 @endforeach
 
 @else
--- routes/web.php
+++ routes/web.php
@@ -24,10 +24,11 @@ Route::get('/', function () {
 });
 
 // Single listing
-Route::get('/listings/{id}', function($id){
+Route::get('/listings/{listing}', function(Listing $listing){
     return view('listing', [
-        'listing' => Listing::find($id)
+        'listing' => $listing
     ]);
+    
 });
 
 // Route::get('/posts/{id}', function($id){

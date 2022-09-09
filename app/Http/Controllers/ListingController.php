890bdb4df529b36e2cfda2cfb0576c0ace10bc30
walumbe <nathanwalumbe@gmail.com>
2022-09-09 18:36:40 +0300

    Implemented pagination

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -13,7 +13,7 @@ class ListingController extends Controller
         // dd(request('tag'));
         return view('listings.index', [
             'listings' => Listing::latest()->
-            filter(request(['tag', 'search']))->get()
+            filter(request(['tag', 'search']))->paginate(2)
         ]);
     }
 
--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -15,4 +15,8 @@
 <p>No Listings found</p>
 @endunless 
 
+</div>
+<div class="mt-6 p-4">
+    {{$listings->links()}}
+</div>
 </x-layout>
